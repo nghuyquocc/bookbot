7d99bcd72968cca020d42912e70029053e965a59
nghuyquocc <nghuyquocc@gmail.com>
2025-06-15 18:01:14 -0700

    some changes

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ from stats import *
 import sys
 
 def main():
-    #print(get_book_text("books/frankenstein.txt").split())
-    #print(f"{number_of_words(get_book_text("books/frankenstein.txt"))} words found in the document")
-    #print(f"{count(get_book_text("books/frankenstein.txt"))}")
     if len(sys.argv) < 2:
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
@@ -18,7 +15,4 @@ def main():
         print("============= END ===============")
 
     
-    # print(sys.argv[0], 'books/frankenstein.txt')
-    # print(sys)
 main()
-
